8678f8d525a9234d59721bd5eb9876ee47164700
AlexSKGHA <41026477+AlexSKGHA@users.noreply.github.com>
2022-02-24 20:35:47 +0400

    Update invman.lua

--- invman.lua
+++ invman.lua
@@ -4,10 +4,52 @@ local ic = component.inventory_controller
 local o = require "os"
 
 local invman = {
-
 	maxSize = robot.inventorySize(),
 	inventory = {}
 }
+
+function invman.get_slots(self, item, force_unveil)
+	
+	if type(item) == "number" then
+		item = self.inventory[item]["label"]
+	end
+
+	slots = {}
+
+	for i = 1, self.maxSize do
+		if force_unveil then
+			if self.inventory[i]["label"] == "undefined" then
+				robot.select(i)
+				self.inventory[i] = ic.getStackInInternalSlot()
+			end
+		end
+
+		if self.inventory[i]["label"] == item then
+			table.insert(slots, i)
+		end
+	end
+	robot.select(1)
+	return slots
+end
+
+function invman.init_toolbelt(self)
+	empty_slots = self:get_slots(nil, false)
+	if #empty_slots == 0 then
+		error("No empty slots availible to initiate toolbelt entry")
+	end
+
+	robot.select(empty_slots[1])
+	ic.equip()
+	stack = ic.getStackInInternalSlot()
+	if stack then
+		self.toolbelt = stack
+	else
+		self.toolbelt = {}
+	end
+
+	ic.equip()
+	robot.select(1)
+end
 
 function invman.init(self)
 	for i = 1, self.maxSize do
@@ -19,14 +61,14 @@ function invman.init(self)
 			self.inventory[i] = {}
 		end
 	end
-	return
+	
+	self:init_toolbelt()
 end
 
 function invman.update(self)
 	
 	for i = 1, self.maxSize do
-		robot.select(i)
-		count = robot.count()
+		count = robot.count(i)
 		stack = self.inventory[i]
 
 		if stack["label"] == nil then
@@ -47,43 +89,47 @@ function invman.update(self)
 	end
 end
 
-function invman.get_slot(self, item, force_unveil)
-	
-	slots = {}
+function invman.equip(self, item)
 
-	for i = 1, self.maxSize do
+	assert(not self.toolbelt["label"], "Slot is occupied")
 
-		if self.inventory[i]["label"] == "undefined" then
-			if force_unveil then
-				robot.select(i)
-				self.inventory[i] = ic.getStackInInternalSlot()
-			end
-		end
-
-		if self.inventory[i]["label"] == item then
-			table.insert(slots, i)
-		end
-	end
+	slot_number = self:get_slots(item)[1]
+	robot.select(slot_number)
+	ic.equip()
+	self.toolbelt = ic.getStackInInternalSlot()
 	robot.select(1)
-	return slots
 end
 
-function invman.init_toolbelt(self)
-	empty_slots = self:get_slot(nil, false)
+function invman.unequip(self)
+	
+	assert (self.toolbelt["label"], "Nothing to unequip")
+
+	empty_slots = self:get_slots(nil, false)
 	if #empty_slots == 0 then
-		error("No empty slots availible to initiate toolbelt entry")
+		error("No at least one empty slot for a tool to unequip")
 	end
 
 	robot.select(empty_slots[1])
 	ic.equip()
-	stack = ic.getStackInInternalSlot()
-	if stack then
-		self.toolbelt = stack
-	else
-		self.toolbelt = {}
-	end
+	self.toolbelt = {}
+	robot.select(1)
+end
 
-	ic.equip()
+function invman.place(self, item, force_unveil)
+	item_slot = self:get_slots(item, force_unveil)[1]
+
+	robot.select(item_slot)
+	is_success = robot.place()
+	if is_success then
+		stack_size = self.inventory[item_slot]["size"]
+		if stack_size == 1 then
+			self.inventory[item_slot] = {}
+		else
+			self.inventory[item_slot]["size"] = stack_size - 1
+		end
+	else
+		error("Item has not placed")
+	end
 	robot.select(1)
 end
 
